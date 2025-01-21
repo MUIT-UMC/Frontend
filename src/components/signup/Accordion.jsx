8a5451bde2efdc752fca4ab9c60b074df9ed4b8d
Remi1001100 <156552154+Remi1001100@users.noreply.github.com>
2025-01-21 16:27:21 +0900

    0121-1 회원가입 페이지 UI (기능 구현 X)

--- src/components/signup/Accordion.jsx
+++ src/components/signup/Accordion.jsx
@@ -1,65 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ToggleUp from "../../assets/icons/ToggleUp.svg";
 import ToggleDown from "../../assets/icons/ToggleDown.svg";
 
 const COLOR_MUIT_RED = "#A00000";
 
-const TermsAccordion = (props) => {
+const TermsAccordion = ({ terms, checkedTerms, onCheckboxChange }) => {
   const [activeIndex, setActiveIndex] = useState(null);
-  const [checkedTerms, setCheckedTerms] = useState({
-    term1: false,
-    term2: false,
-    term3: false,
-    term4: false,
-    term5: false,
-  });
 
   const toggleAccordion = (index) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
-  const handleCheckboxChange = (id) => {
-    setCheckedTerms((prev) => {
-      const updated = { ...prev, [id]: !prev[id] };
-      updated.all = Object.values(updated).slice(1).every((checked) => checked);
-      return updated;
-    });
-  };
-
   return (
     <Container>
-      {props?.terms.map((term, index) => (
+      {terms.map((term, index) => (
         <Accordion key={term.id}>
           <input
             type="checkbox"
             id={term.id}
             className="agree-chk"
             checked={checkedTerms[term.id]}
-            onChange={() => handleCheckboxChange(term.id)}
+            onChange={() => onCheckboxChange(term.id)}
           />
           <BorderArea>
             <AccordionHeader>
               <label htmlFor={term.id}>{term.title}</label>
               <ToggleButton onClick={() => toggleAccordion(index)}>
-                {activeIndex === index ? <img src={ToggleUp}/> : <img src={ToggleDown}/>}
+                {activeIndex === index ? (
+                  <img src={ToggleUp} />
+                ) : (
+                  <img src={ToggleDown} />
+                )}
               </ToggleButton>
             </AccordionHeader>
-
             {activeIndex === index && (
               <AccordionContent>
                 <p>{term.content}</p>
               </AccordionContent>
             )}
           </BorderArea>
-
         </Accordion>
       ))}
     </Container>
   );
 };
 
-// Styled components
+
 const Container = styled.div`
   width: 100%;
 `;
