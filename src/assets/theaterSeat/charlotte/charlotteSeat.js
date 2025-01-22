f1d88f900681cd30f3fd9f82f025928e06a95237
Remi1001100 <156552154+Remi1001100@users.noreply.github.com>
2025-01-22 15:17:18 +0900

    0122-1

---
+++ src/assets/theaterSeat/charlotte/charlotteSeat.js
@@ -0,0 +1,65 @@
+import A_Before from "./Before/A.png";
+import B_Before from "./Before/B.png";
+import C_Before from "./Before/C.png";
+import D_Before from "./Before/D.png";
+import E_Before from "./Before/E.png";
+import F_Before from "./Before/F.png";
+import G_Before from "./Before/G.png";
+import H_Before from "./Before/I.png";
+import I_Before from "./Before/I.png";
+import J_Before from "./Before/J.png";
+import K_Before from "./Before/K.png";
+import L_Before from "./Before/L.png";
+import M_Before from "./Before/M.png";
+import N_Before from "./Before/N.png";
+import O_Before from "./Before/O.png";
+
+import A_After from "./After/A.png";
+import B_After from "./After/B.png";
+import C_After from "./After/C.png";
+import D_After from "./After/D.png";
+import E_After from "./After/E.png";
+import F_After from "./After/F.png";
+import G_After from "./After/G.png";
+import H_After from "./After/H.png";
+import I_After from "./After/I.png";
+import J_After from "./After/J.png";
+import K_After from "./After/K.png";
+import L_After from "./After/L.png";
+import M_After from "./After/M.png";
+import N_After from "./After/N.png";
+import O_After from "./After/O.png";
+
+export {
+    A_Before,
+    B_Before,
+    C_Before,
+    D_Before,
+    E_Before,
+    F_Before,
+    G_Before,
+    H_Before,
+    I_Before,
+    J_Before,
+    K_Before,
+    L_Before,
+    M_Before,
+    N_Before,
+    O_Before,
+    A_After,
+    B_After,
+    C_After,
+    D_After,
+    E_After,
+    F_After,
+    G_After,
+    H_After,
+    I_After,
+    J_After,
+    K_After,
+    L_After,
+    M_After,
+    N_After,
+    O_After,
+
+};
