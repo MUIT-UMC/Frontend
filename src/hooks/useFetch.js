d3c8b65227b3803e587b58cc708a87267b9adcca
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2025-01-28 00:44:49 +0900

    [CHORE] api 테스트용 페이지 추가 및 useFetch 커스텀 훅 구현

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('네트워크 응답 실패');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
