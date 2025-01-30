de76085f38c689ab8fa369b7c05b69e0cf0008bc
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2025-01-31 01:44:24 +0900

    [CHORE] 내 게시글: 예시 데이터 수정

--- src/pages/mypage/my/MyPosts.jsx
+++ src/pages/mypage/my/MyPosts.jsx
@@ -8,14 +8,13 @@ function MyPosts() {
     "제목", "게시판", "날짜"
   ]
   const details = [
-    { id:1, name: "알라딘 어떤거 같아?", musical: "익명 게시판", place: "2025-01-15 (수)"},
-    { id:2, name: "알라딘 관람 후기", musical: "리뷰 게시판", place: "2025-01-05 (수)"},
-    { id:3,  name: "실종", musical: "소극장 연극아", place: "2024-10-01 (화)"},
+    { id:1, title: "알라딘 어떤거 같아?", musicalName: "익명 게시판", location: "2025-01-15 (수)"},
+    { id:2, title: "알라딘 관람 후기", musicalName: "리뷰 게시판", location: "2025-01-05 (수)"},
+    { id:3,  title: "실종", musicalName: "소극장 연극아", location: "2024-10-01 (화)"},
   ];
   
   return (
     <>
-      <h1>내가 쓴 글</h1>
       <PostList details={details} headers={tableHeaders} cols={3}/>
     </>
   );
