9beddb1d89473b5da10d80a5b018298865a1e1e8
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2025-02-21 01:05:50 +0900

    [FEAT] 디테일페이지 스타일 수정, 하위페이지의 데이터가 없을 경우의 처리

--- src/pages/detail/subpages/Performance.jsx
+++ src/pages/detail/subpages/Performance.jsx
@@ -8,10 +8,19 @@ function Performance({ data }) {
   return (
     
     <>
-      <img src={data?.result?.desImgUrl[0]} />
+     {data?.result?.desImgUrl[0] ? (
+        <Img src={data?.result?.desImgUrl[0]} />
+      ) : (
+        <div>공연정보가 없습니다.</div>
+      )
+    }
    </>
     
   );
 }
 
 export default Performance;
+
+const Img = styled.img`
+  max-width: 800px;
+`
